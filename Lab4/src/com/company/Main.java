b6e0dc440f3212b62a80886ccf79249124ff0ac1
Grigorii Burdin <birdingri@gmail.com>
2017-04-13 22:47:52 +0300

    changePrice added

--- Lab4/src/com/company/Main.java
+++ Lab4/src/com/company/Main.java
@@ -5,7 +5,6 @@ public class Main {
         Product product = new Product("Edam", 3.3, 120);
         System.out.println("Product value is " + product.countValue());
         product.printProduct();
-        System.out.println();
         product.changePrice();
     }
 }
